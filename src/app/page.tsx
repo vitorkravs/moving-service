3c1d3eec47bed941abf59c60b9f29b7df0e3bac1
vitor kravszenko <vitorkravs3@gmail.com>
2024-04-02 00:07:05 -0300

    feat: adicionando componente Header na page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Header from "@/components/Header/Header";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <>
+      <Header />
+      <main></main>
+    </>
+  );
 }
