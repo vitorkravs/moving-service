cfa6f7a5e1699d5262b1970850a3b893a5711d14
vitor kravszenko <vitorkravs3@gmail.com>
2024-04-02 18:36:57 -0300

    feat: criando estrutura do componente Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+//style
+import "./styles.scss";
+
+const Footer = () => {
+  return (
+    <footer id="footer-container">
+      <p>Feito por Vitor cesar Kravszenko</p>
+    </footer>
+  );
+};
+
+export default Footer;
+<footer></footer>;
