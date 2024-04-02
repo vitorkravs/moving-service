d31f7c4b7d50fcc329d208027d67f45e65cbb740
vitor kravszenko <vitorkravs3@gmail.com>
2024-04-02 16:29:12 -0300

    feat: adicionando componente AboutUs na page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import AboutUs from "@/components/AboutUs/AboutUs";
 import Header from "@/components/Header/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <AboutUs />
+      </main>
     </>
   );
 }
